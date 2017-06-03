f91666f1deb8f0a6c4c213542a9cd34e79a625db
KeF1R <KeF1R@DESKTOP-H0RUO92>
2017-06-03 15:51:42 +0400

    Добавьте файлы проекта.

---
+++ Currency parser/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Currency_parser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.NI_icon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.CMS_menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.authorSiteToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.CMS_menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NI_icon
+            // 
+            this.NI_icon.ContextMenuStrip = this.CMS_menu;
+            this.NI_icon.Icon = ((System.Drawing.Icon)(resources.GetObject("NI_icon.Icon")));
+            this.NI_icon.Visible = true;
+            // 
+            // CMS_menu
+            // 
+            this.CMS_menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshToolStripMenuItem1,
+            this.authorSiteToolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.CMS_menu.Name = "contextMenuStrip1";
+            this.CMS_menu.Size = new System.Drawing.Size(134, 70);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // refreshToolStripMenuItem1
+            // 
+            this.refreshToolStripMenuItem1.Name = "refreshToolStripMenuItem1";
+            this.refreshToolStripMenuItem1.Size = new System.Drawing.Size(133, 22);
+            this.refreshToolStripMenuItem1.Text = "Refresh";
+            this.refreshToolStripMenuItem1.Click += new System.EventHandler(this.refreshToolStripMenuItem1_Click);
+            // 
+            // authorSiteToolStripMenuItem1
+            // 
+            this.authorSiteToolStripMenuItem1.Name = "authorSiteToolStripMenuItem1";
+            this.authorSiteToolStripMenuItem1.Size = new System.Drawing.Size(133, 22);
+            this.authorSiteToolStripMenuItem1.Text = "Author Site";
+            this.authorSiteToolStripMenuItem1.Click += new System.EventHandler(this.authorSiteToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(147, 108);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowInTaskbar = false;
+            this.Text = "Exchange Rates";
+            this.CMS_menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon NI_icon;
+        private System.Windows.Forms.ContextMenuStrip CMS_menu;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem authorSiteToolStripMenuItem1;
+    }
+}
+
---
+++ Currency parser/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Currency_parser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            refresh();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        void refresh()
+        {
+            try
+            {
+                System.Net.WebClient WC = new System.Net.WebClient();
+                //([0-9]+\.[0-9]+)
+                string usd = "USD: " + System.Text.RegularExpressions.Regex.Match(
+                    WC.DownloadString("https://www.sberometer.ru/cbr/"),
+                    @"<span class=""bcsblock__value"" id=""bcs_usd_sell"">([0-9]+\,[0-9]+)</span>").Groups[1].Value;
+                string eur = " EUR: " + System.Text.RegularExpressions.Regex.Match(
+                WC.DownloadString("https://www.sberometer.ru/cbr/"),
+                @"<span class=""bcsblock__value"" id=""bcs_eur_sell"">([0-9]+\,[0-9]+)</span>").Groups[1].Value;
+                NI_icon.Text = usd + eur;
+            }
+            catch { }
+        }
+        private void refreshToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+        private void authorSiteToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://kef1r.ru");
+        }
+    }
+}
